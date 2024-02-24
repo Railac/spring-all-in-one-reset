aead70a0204183ef165004f517d4bfd495b21038
Railac <urur700@naver.com>
2024-02-25 00:13:32 +0900

    게시글 수정,삭제

--- src/main/java/com/test/springboot/service/posts/PostsService.java
+++ src/main/java/com/test/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.test.springboot.service.posts;
 
 import com.test.springboot.domain.posts.Posts;
 import com.test.springboot.domain.posts.PostsRepository;
+import com.test.springboot.web.dto.PostsListResponseDto;
 import com.test.springboot.web.dto.PostsResponseDto;
 import com.test.springboot.web.dto.PostsSaveRequestDto;
 import com.test.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor //필요한 생성자 생성해줌 여기선 final 필드
 @Service
@@ -34,4 +38,19 @@ public class PostsService {
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true) //조회속도가 개선됨, 트랜잭션을 왜거는지 생각했을떄 readOnly만하면 장점을 생각하면될듯.
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
 }
--- src/main/java/com/test/springboot/web/PostApiController.java
+++ src/main/java/com/test/springboot/web/PostApiController.java
@@ -13,12 +13,12 @@ public class PostApiController {
 
     private final PostsService postsService;
 
-    @PostMapping("/api/v1/posts")
+    @PostMapping("/api/v1/posts") //저장
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
     }
 
-    @PutMapping("/api/v1/posts/{id}")
+    @PutMapping("/api/v1/posts/{id}") //update
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
         return postsService.update(id, requestDto);
     }
@@ -26,5 +26,11 @@ public class PostApiController {
     @GetMapping("/api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id){
         return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        postsService.delete(id);
+        return id;
     }
 }

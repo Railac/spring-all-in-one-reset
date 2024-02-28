cbc7573ac9ff989d24e4610372bf550fd7452cb1
Railac <urur700@naver.com>
2024-02-29 00:03:54 +0900

    네이버로그인 , 시큐리티 TEST 오류 해결(재시작으로해결)

--- src/test/java/com/test/springboot/web/HelloControllerTest.java
+++ src/test/java/com/test/springboot/web/HelloControllerTest.java
@@ -1,12 +1,16 @@
 package com.test.springboot.web;
 
 
+import com.test.springboot.config.auth.SecurityConfig;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,12 +23,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 
 @ExtendWith(SpringExtension.class) //테스트시 JUnit 내장 말고 지정해준 실행자로 테스트
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
 public class HelloControllerTest {
 
     @Autowired private MockMvc mvc;
 
     @Test
+    @WithMockUser(roles = "USER")
     @DisplayName("hello가_리턴된다")
     public void hello() throws Exception {
         String hello = "hello";
@@ -34,6 +43,7 @@ public class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser(roles = "USER")
     public void helloDto가_린턴된다() throws Exception{
         String name = "hello";
         int amount = 1000;
